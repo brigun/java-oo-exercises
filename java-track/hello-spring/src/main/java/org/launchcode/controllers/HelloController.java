76e75465dbddff79a4ab65edba1183ffbd38f26d
Brian Gunter <brigun@gmail.com>
2016-10-29 19:21:23 -0500

    Hello, world! in spring-boot, takes a get parameter
    and returns a web page.

---
+++ java-track/hello-spring/src/main/java/org/launchcode/controllers/HelloController.java
@@ -0,0 +1,25 @@
+package org.launchcode.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.launchcode.models.HelloMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController
+{
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@ResponseBody
+	public String hello(HttpServletRequest request)
+	{
+		String name = request.getParameter("name");
+		
+		if(name == null) name = "world";
+			
+		return "<h1>" + HelloMessage.getMessage(name) + "</h1>";
+	}
+
+}
---
+++ java-track/hello-spring/src/main/java/org/launchcode/models/HelloMessage.java
@@ -0,0 +1,11 @@
+package org.launchcode.models;
+
+public class HelloMessage 
+{
+	
+	public static String getMessage(String name)
+	{
+		return "Hello, " + name +"!";
+	}
+
+}
