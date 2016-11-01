a781733070988aa02c49c1ce8b2dedba6923c6e4
Brian Gunter <brigun@gmail.com>
2016-10-31 20:36:13 -0500

    Springframework.guru webapp, added feature web output (bare bones)

---
+++ java-track/webappwithspringboot/src/main/java/guru/springframework/WebappwithspringbootApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappwithspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappwithspringbootApplication.class, args);
+	}
+}
