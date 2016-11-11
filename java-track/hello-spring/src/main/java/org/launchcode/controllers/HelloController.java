b98f884cead57c12c1939e66a2fc0599a6e946ba
Brian Gunter <brigun@gmail.com>
2016-11-10 19:51:38 -0600

    Added persistance.

--- java-track/hello-spring/src/main/java/org/launchcode/controllers/HelloController.java
+++ java-track/hello-spring/src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,8 +1,13 @@
 package org.launchcode.controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.launchcode.models.HelloLog;
 import org.launchcode.models.HelloMessage;
+import org.launchcode.models.dao.HelloLogDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloController 
 {
+	@Autowired
+	private HelloLogDao helloLogDao;
+	
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String helloForm()
 	{
@@ -27,9 +35,23 @@ public class HelloController
 		
 		if(name == null || name == "") name = "World";
 		
+		HelloLog log = new HelloLog(name);
+		
+		helloLogDao.save(log);
+		
 		model.addAttribute("message",  HelloMessage.getMessage(name, language));
 		
 		return "hello";
+		
+	}
+	
+	@RequestMapping(value = "/log")
+	public String log(Model model)
+	{
+		List <HelloLog> logs = helloLogDao.findAll();
+		
+		model.addAttribute("logs", logs);
+		return "log";
 		
 	}
 
